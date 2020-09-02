18752494c034973d427e3104f5615265fe1f6db9
Emerson-Duarte <pduarte.emerson@gmail.com>
2020-09-02 19:50:05 -0300

    translate to pt-BR:  Model Contact

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.6.6'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 #This gem is a port of Perl's Data::Faker library that generates fake data.
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+#Centralization of locale data collection for Ruby on Rails.
+gem 'rails-i18n', '~> 6.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Notebook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+# sets the default locale of an application used for i18n
+    I18n.default_locale = :pt
+    config.i18n.default_locale = :pt
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
